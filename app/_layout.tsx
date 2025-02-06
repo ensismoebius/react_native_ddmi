1037e97406cc3a29b4713bc287261541f128c2e3
André Furlan <ensismoebius@gmail.com>
2025-02-06 13:35:39 -0300

    Componente SafeAreProvider adicionado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
+// Componente que garante que o conteúdo seja mostrado em uma
+// área "segura", ou seja que não seja coberta por algum outro 
+// elemento da interface gráfica
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
---
+++ data/todos.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        "id": 0,
+        "title": "Organizar a sociedade",
+        "completed": false
+    },
+    {
+        "id": 1,
+        "title": "Ampliar direitos",
+        "completed": false
+    },
+    {
+        "id": 2,
+        "title": "Tomar os meios de produção",
+        "completed": false
+    },
+    {
+        "id": 3,
+        "title": "Implantar autogestão",
+        "completed": false
+    }
+];

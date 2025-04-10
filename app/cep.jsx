44b2bf0da1918c28973a97171790682d7e209775
André Furlan <ensismoebius@gmail.com>
2025-04-10 13:48:20 -0300

    Buscando o cep

--- app/cep.jsx
+++ app/cep.jsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 import { TextInput } from "react-native";
 import { View } from "react-native";
 import { ActivityIndicator } from "react-native";
 
 export default function cep()
 {
-    const { valorDoCep, setValorDoCep } = useState("");
-    const { carregando, setCarrregando } = useState(false);
+    const [valorDoCep, setValorDoCep] = useState('');
+    const [carregando, setCarregando] = useState(false);
+    const [dados, setDados] = useState(null);
+
+    async function carregarDadosDoCEP()
+    {
+        if (valorDoCep.length !== 8)
+        {
+            setValorDoCep("CEP inválido");
+            return;
+        }
+
+        setCarregando(true);
+
+
+        await fetch(`https://viacep.com.br/ws/${valorDoCep}/json/`)
+            .then(
+                dadoEmJson =>
+                {
+                    if (dadoEmJson.erro)
+                    {
+                        setValorDoCep("CEP não existe");
+                    } else
+                    {
+                        // Deu certo
+                        setDados(dadoEmJson);
+                    }
+                }
+            );
+
+
+    }
 
     return (
         <View>
@@ -18,7 +48,16 @@ export default function cep()
                 keyboardType="numeric"
                 maxLength={8}
             />
+
             {carregando && <ActivityIndicator />}
+
+            {dados &&
+                <View>
+                    <Text>Endereço: {JSON.stringify(dados)}</Text>
+                </View>
+            }
+
+            <Button title="Consultar" onPress={carregarDadosDoCEP} />
 
         </View>
     );
